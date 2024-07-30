accf9f25321a74353a19a529167366a148ce3205
DevTCode <128856806+DevTCode@users.noreply.github.com>
2024-07-30 20:53:25 +0000

    Create Event.java

---
+++ src/main/java/com/event/GestionEvents/Entity/Event.java
@@ -0,0 +1,131 @@
+package com.event.GestionEvents.Entity;
+
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "evenement")
+public class Event {
+    @Id
+    //primary et auto-increment
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int event_id;
+
+    private String nom;
+
+    private String description;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+    private String heure;
+
+    private String type;
+
+    private String lieu;
+
+    private int duree;
+    private int placesDisponibles;
+    @OneToMany(mappedBy = "evenement")
+    private List<Reservation> reservations;
+    private String chemin_image;
+
+    private String etat;
+
+    public int getPlacesDisponibles() {
+        return placesDisponibles;
+    }
+
+    public void setPlacesDisponibles(int placesDisponibles) {
+        this.placesDisponibles = placesDisponibles;
+    }
+
+    public int getEvent_id() {
+        return event_id;
+    }
+
+    public void setEvent_id(int event_id) {
+        this.event_id = event_id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getHeure() {
+        return heure;
+    }
+
+    public void setHeure(String heure) {
+        this.heure = heure;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLieu() {
+        return lieu;
+    }
+
+    public void setLieu(String lieu) {
+        this.lieu = lieu;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+    public String getChemin_image() {
+        return chemin_image;
+    }
+
+    public void setChemin_image(String chemin_image) {
+        this.chemin_image = chemin_image;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
+}
