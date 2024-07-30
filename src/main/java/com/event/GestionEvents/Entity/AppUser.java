a49695de9258ed3198892047b551e6743c81df89
DevTCode <128856806+DevTCode@users.noreply.github.com>
2024-07-30 20:53:19 +0000

    Create AppUser.java

---
+++ src/main/java/com/event/GestionEvents/Entity/AppUser.java
@@ -0,0 +1,65 @@
+package com.event.GestionEvents.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "utilisateur")
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @OneToOne(mappedBy = "utilisateur", cascade = CascadeType.ALL)
+    private Employe employe;
+
+    // Getters et Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Employe getEmploye() {
+        return employe;
+    }
+
+    public void setEmploye(Employe employe) {
+        this.employe = employe;
+    }
+}
